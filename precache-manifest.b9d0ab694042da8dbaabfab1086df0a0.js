2b5fa54f2e9b864a3f161e3f711fd52e46a569cb
SB067746 <SB067746@cerner.net>
2020-03-25 13:22:17 +0530

    Updates

---
+++ precache-manifest.b9d0ab694042da8dbaabfab1086df0a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec34d4ce648f3f30641434583d6424b",
+    "url": "/India-corona-tracker%7D/index.html"
+  },
+  {
+    "revision": "0fe146fcf2a69c038453",
+    "url": "/India-corona-tracker%7D/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8ca8a1e23d16c4f568cd",
+    "url": "/India-corona-tracker%7D/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0fe146fcf2a69c038453",
+    "url": "/India-corona-tracker%7D/static/js/2.d5e98258.chunk.js"
+  },
+  {
+    "revision": "1035eb16e0f287e503e9fbe9e7321198",
+    "url": "/India-corona-tracker%7D/static/js/2.d5e98258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca8a1e23d16c4f568cd",
+    "url": "/India-corona-tracker%7D/static/js/main.ef37395c.chunk.js"
+  },
+  {
+    "revision": "0879258571e190d4dd88",
+    "url": "/India-corona-tracker%7D/static/js/runtime-main.cc4b0249.js"
+  }
+]);
